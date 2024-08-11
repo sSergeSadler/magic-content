d789bbf796f3c77cb85db7675965699b3659050f
SadlerSerge <74461163+sSergeSadler@users.noreply.github.com>
2024-08-11 10:41:11 +0000

    Commit update gemini api

--- src/lib/gemini-ai.ts
+++ src/lib/gemini-ai.ts
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const apiKey = process.env.GEMINI_API_KEY;
-const genAI = new GoogleGenerativeAI(apiKey as string);
+const apiKey = "AIzaSyCt4Gre6zguke3UcUc1kkMgrzqGJDd3lnQ";
+const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
