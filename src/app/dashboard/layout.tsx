20797c6f61d04588c16cea8fac12c41bd1021435
SadlerSerge <74461163+sSergeSadler@users.noreply.github.com>
2024-08-11 14:10:35 +0000

    Commit remove chart

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -9,7 +9,6 @@ const DashboardLayout = ({
     <div className="bg-gray-50 h-screen">
       <div className="md:w-64 hidden md:block fixed">
         <Sidebar />
-        <AIUsage />
       </div>
       <div className="md:ml-64 bg-gray-50 h-fit pb-5">{children}</div>
     </div>
